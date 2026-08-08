8b5fa53d374c22b70e76028bc2fc156bdc30e1a8
mbilal565649-webdeveloper <mbilal565649@gmail.com>
2026-08-08 15:59:51 +0500

    Add Image Search App

---
+++ script.js
@@ -0,0 +1,35 @@
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const imageContainer = document.getElementById("imageContainer");
+
+async function searchImages() {
+  const searchTerm = searchInput.value.trim();
+
+  if (searchTerm === "") {
+    alert("Please enter an image name");
+    return;
+  }
+
+  console.log(searchTerm);
+
+  const url = `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=l8dXGulJhnuYqw9PUWJQgth8f--S0YNAiXBS7BvORSM`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+
+  let images = "";
+
+  data.results.forEach(function (photo) {
+    images += `
+        <div class="imageCard">
+            <img src="${photo.urls.small}" alt="${photo.alt_description || "Image"}">
+        </div>
+    `;
+  });
+
+  imageContainer.innerHTML = images;
+}
+
+searchBtn.addEventListener("click", searchImages);
